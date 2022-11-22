edd8133a02aa76f92e19445d14b9da03d3a54ca4
Juanxpeke <juanintendo.f11@gmail.com>
2022-11-21 21:00:04 -0300

    Some refactoring

--- Tests/TileMap.gd
+++ Tests/TileMap.gd
@@ -1,8 +1,9 @@
 extends TileMap
 class_name AStarTileMap
 
-# Wrapper class for TileMap class. It adds new basic functionalities and also
-# allows the user to draw paths or areas in it.
+# Wrapper class for TileMap class. It adds new basic functionalities using
+# different methods and a AStar inner object, allowing the user to draw paths 
+# or areas in it.
 
 const DIRECTIONS := [Vector2.RIGHT, Vector2.UP, Vector2.LEFT, Vector2.DOWN]
 
@@ -74,80 +75,10 @@ func get_used_cells_origins() -> Array:
 func get_cells_path(start_origin : Vector2, end_origin : Vector2) -> Array:
 	if not has_cell(start_origin) or not has_cell(end_origin): return []
 	return astar.get_point_path(get_cell_id(start_origin), get_cell_id(end_origin)) as Array
-
-# Returns an array with the coordinates of cells in the discrete line from
-# coordinates (x0, y0) to (x1, y1). Low part of Bresenham's line algorithm.
-func get_cells_line_low(x0 : int, y0 : int, x1 : int, y1 : int) -> Array:
-	var cells_line = []
-	var dx : int = x1 - x0
-	var dy : int = y1 - y0
-	var yi : int = 1
-	
-	if dy < 0:
-		yi = -1
-		dy = -dy
 		
-	var D : int = (2 * dy) - dx
-	var y : int = y0
-
-	for x in range(x0, x1 + 1):
-		cells_line.append(Vector2(x, y))
-		if D > 0:
-			y = y + yi
-			D = D + (2 * (dy - dx))
-		else:
-			D = D + 2 * dy
-			
-	return cells_line
-
 # Returns an array with the coordinates of cells in the discrete line from
-# coordinates (x0, y0) to (x1, y1). High part of Bresenham's line algorithm.
-func get_cells_line_high(x0 : int, y0 : int, x1 : int, y1 : int) -> Array:
-	var cells_line = []
-	var dx : int = x1 - x0
-	var dy : int = y1 - y0
-	var xi : int = 1
-	
-	if dx < 0:
-		xi = -1
-		dx = -dx
-
-	var D : int = (2 * dx) - dy
-	var x : int = x0
-
-	for y in range(y0, y1 + 1):
-		cells_line.append(Vector2(x, y))
-		if D > 0:
-			x = x + xi
-			D = D + (2 * (dx - dy))
-		else:
-			D = D + 2 * dx
-			
-	return cells_line
-
-# Returns an array with the coordinates of cells in the discrete line from 
-# start_coord to end_coord, it uses the Bresenham's line algorithm divided
-# in different cases.
-func get_cells_line(start_coord : Vector2, end_coord : Vector2) -> Array:
-	var x0 : int = start_coord[0]
-	var y0 : int = start_coord[1]
-	var x1 : int = end_coord[0]
-	var y1 : int = end_coord[1]
-	
-	if abs(y1 - y0) < abs(x1 - x0):
-		if x0 > x1:
-			return get_cells_line_low(x1, y1, x0, y0)
-		else:
-			return get_cells_line_low(x0, y0, x1, y1)
-	else:
-		if y0 > y1:
-			return get_cells_line_high(x1, y1, x0, y0)
-		else:
-			return get_cells_line_high(x0, y0, x1, y1)
-	
-# Returns an array with the coordinates of cells in the discrete line from
-# coordinates (x0, y0) to (x1, y1). Low part of Bresenham's line algorithm.
-func get_jcells_line_low(x0 : int, y0 : int, x1 : int, y1 : int) -> Array:
+# coordinates (x0, y0) to (x1, y1). Low part of modified Dofus' line algorithm.
+func get_cells_line_low(x0 : int, y0 : int, x1 : int, y1 : int) -> Array:
 	var cells_line = []
 	var dx : int = x1 - x0
 	var dy : int = y1 - y0
@@ -175,8 +106,8 @@ func get_jcells_line_low(x0 : int, y0 : int, x1 : int, y1 : int) -> Array:
 	return cells_line
 
 # Returns an array with the coordinates of cells in the discrete line from
-# coordinates (x0, y0) to (x1, y1). High part of Bresenham's line algorithm.
-func get_jcells_line_high(x0 : int, y0 : int, x1 : int, y1 : int) -> Array:
+# coordinates (x0, y0) to (x1, y1). High part of modified Dofus' line algorithm.
+func get_cells_line_high(x0 : int, y0 : int, x1 : int, y1 : int) -> Array:
 	var cells_line = []
 	var dx : int = x1 - x0
 	var dy : int = y1 - y0
@@ -204,9 +135,9 @@ func get_jcells_line_high(x0 : int, y0 : int, x1 : int, y1 : int) -> Array:
 	return cells_line
 
 # Returns an array with the coordinates of cells in the discrete line from 
-# start_coord to end_coord, it uses the Bresenham's line algorithm divided
-# in different cases.
-func get_jcells_line(start_coord : Vector2, end_coord : Vector2) -> Array:
+# start_coord to end_coord, it uses a modified version of the Bresenham's line 
+# algorithm divided in different cases, trying to replicate the Dofus' behaviour.
+func get_cells_line(start_coord : Vector2, end_coord : Vector2) -> Array:
 	var x0 : int = start_coord[0]
 	var y0 : int = start_coord[1]
 	var x1 : int = end_coord[0]
@@ -214,14 +145,14 @@ func get_jcells_line(start_coord : Vector2, end_coord : Vector2) -> Array:
 	
 	if abs(y1 - y0) < abs(x1 - x0):
 		if x0 > x1:
-			return get_jcells_line_low(x1, y1, x0, y0)
+			return get_cells_line_low(x1, y1, x0, y0)
 		else:
-			return get_jcells_line_low(x0, y0, x1, y1)
+			return get_cells_line_low(x0, y0, x1, y1)
 	else:
 		if y0 > y1:
-			return get_jcells_line_high(x1, y1, x0, y0)
+			return get_cells_line_high(x1, y1, x0, y0)
 		else:
-			return get_jcells_line_high(x0, y0, x1, y1)
+			return get_cells_line_high(x0, y0, x1, y1)
 	
 # Given a cell origin, returns a unique identifier. It uses improved Szudzik pair 
 # algorithm to calculate the ID, and first transforms the origin to the equivalent
@@ -239,8 +170,7 @@ func get_cell_id(cell_origin : Vector2) -> int:
 		return -c - 1
 	
 	return c
-	
-	
+		
 # Given a cell origin, it returns the equivalent cartesian coordinate.
 func get_cell_coord(cell_origin : Vector2) -> Vector2:
 	return world_to_map(to_local(cell_origin))
@@ -293,7 +223,7 @@ func show_spell_cells(spell : Spell, origin : Vector2) -> void:
 	
 	for spell_cell_coord in spell_cells:
 		if spell_cell_coord in used_cells:
-			var spell_line_cells := get_jcells_line(origin_coord, spell_cell_coord)
+			var spell_line_cells := get_cells_line(origin_coord, spell_cell_coord)
 			var is_cell_blocked := false
 			for obstacle in obstacles:
 				if get_cell_coord(obstacle.global_position) in spell_line_cells:
@@ -478,3 +408,77 @@ func get_cells_path_avoiding_obstacles(start_position: Vector2, end_position: Ve
 	return []
 	# return set_path_length(astar_path, max_distance)
 
+
+# ===========================
+# ===== EXTRA UTILITIES =====
+# ===========================
+
+# Returns an array with the coordinates of cells in the discrete line from
+# coordinates (x0, y0) to (x1, y1). Low part of Bresenham's line algorithm.
+func get_cells_line_low_b(x0 : int, y0 : int, x1 : int, y1 : int) -> Array:
+	var cells_line = []
+	var dx : int = x1 - x0
+	var dy : int = y1 - y0
+	var yi : int = 1
+	
+	if dy < 0:
+		yi = -1
+		dy = -dy
+		
+	var D : int = (2 * dy) - dx
+	var y : int = y0
+
+	for x in range(x0, x1 + 1):
+		cells_line.append(Vector2(x, y))
+		if D > 0:
+			y = y + yi
+			D = D + (2 * (dy - dx))
+		else:
+			D = D + 2 * dy
+			
+	return cells_line
+
+# Returns an array with the coordinates of cells in the discrete line from
+# coordinates (x0, y0) to (x1, y1). High part of Bresenham's line algorithm.
+func get_cells_line_high_b(x0 : int, y0 : int, x1 : int, y1 : int) -> Array:
+	var cells_line = []
+	var dx : int = x1 - x0
+	var dy : int = y1 - y0
+	var xi : int = 1
+	
+	if dx < 0:
+		xi = -1
+		dx = -dx
+
+	var D : int = (2 * dx) - dy
+	var x : int = x0
+
+	for y in range(y0, y1 + 1):
+		cells_line.append(Vector2(x, y))
+		if D > 0:
+			x = x + xi
+			D = D + (2 * (dx - dy))
+		else:
+			D = D + 2 * dx
+			
+	return cells_line
+
+# Returns an array with the coordinates of cells in the discrete line from 
+# start_coord to end_coord, it uses the Bresenham's line algorithm divided
+# in different cases.
+func get_cells_line_b(start_coord : Vector2, end_coord : Vector2) -> Array:
+	var x0 : int = start_coord[0]
+	var y0 : int = start_coord[1]
+	var x1 : int = end_coord[0]
+	var y1 : int = end_coord[1]
+	
+	if abs(y1 - y0) < abs(x1 - x0):
+		if x0 > x1:
+			return get_cells_line_low_b(x1, y1, x0, y0)
+		else:
+			return get_cells_line_low_b(x0, y0, x1, y1)
+	else:
+		if y0 > y1:
+			return get_cells_line_high_b(x1, y1, x0, y0)
+		else:
+			return get_cells_line_high_b(x0, y0, x1, y1)
